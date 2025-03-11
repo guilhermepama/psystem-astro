98e855df77342e3b8dd79acf093f5cda28ee1bb7
guilhermepama <guilhermepama1@gmail.com>
2025-03-11 16:02:34 -0300

    coniguração paginas clinicas [slug]

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 declare namespace App {
- 
   interface Locals {
     id: string;
     nome: string;
     email: string;
     isAuthenticated: boolean;
-   
+    clinicSlug?: string;
   }
 }
